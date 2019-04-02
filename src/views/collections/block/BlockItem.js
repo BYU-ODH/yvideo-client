d27549c97feefd4a1fa31049159493417362eb01
clevergrant <clevergrant@gmail.com>
2019-04-02 11:23:57 -0600

    fixed the merge

--- src/views/collections/block/BlockItem.js
+++ src/views/collections/block/BlockItem.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
+
 import { ItemContainer, Thumbnail } from './styles.js'
 
-const BlockItem = props => {
-	const { name, thumbnail } = { ...props.data }
-	return (
-		<ItemContainer>
-			{thumbnail ?
-				<Thumbnail className='src' src={thumbnail} />
-				:
-				<Thumbnail className='default' />
-			}
-			<h4>{name}</h4>
-		</ItemContainer>
-	)
+class BlockItem extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			img: props.data.thumbnail,
+			loaded: false
+		}
+	}
+
+	componentDidMount = () => {
+		const temp = new Image()
+		temp.src = this.state.img
+		temp.onload = () => {
+			this.setState({ loaded: true })
+		}
+	}
+
+	render() {
+		const { name, thumbnail } = this.props.data
+		const { loaded } = this.state
+		return (
+			<ItemContainer>
+				<Thumbnail src={thumbnail} loaded={loaded} />
+				<h4>{name}</h4>
+			</ItemContainer>
+		)
+	}
 }
 
 export default BlockItem
