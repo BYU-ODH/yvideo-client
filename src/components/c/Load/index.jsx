d670efa684f37e35a629f12dad92f90511bfe5fa
anita908 <anita248846@gmail.com>
2020-08-18 16:56:51 -0600

    get loading icon working

--- src/components/c/Load/index.jsx
+++ src/components/c/Load/index.jsx
@@ -1,28 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import ReactDOM from 'react-dom'
 
 import { Wrapper, Spinner } from './styles'
 
 class Load extends Component {
 
+	wrapper = createRef()
+
 	render() {
 		return ReactDOM.createPortal(
 			(
-				<Wrapper ref={this.wrapper} className='hidden'>
+				<Wrapper ref={this.wrapper} className='active'>
 					<Spinner />
 				</Wrapper>
-			)
-			,
-			document.getElementById('load'))
+			),
+			document.getElementById(`load`)
+		)
 	}
 
 	componentDidUpdate = prevProps => {
 
-		console.log(this.props)
+		if (!this.wrapper.current) return
 
 		if (!prevProps.loading && this.props.loading) {
-			this.wrapper.current.classList.add('active')
-			this.wrapper.current.classList.remove('hidden')
+			this.wrapper.current.classList.add(`active`)
+			this.wrapper.current.classList.remove(`hidden`)
+		}
+
+		if (prevProps.loading && !this.props.loading) {
+			setTimeout(() => {
+				this.wrapper.current.classList.remove(`active`)
+				setTimeout(() => {
+					this.wrapper.current.classList.add(`hidden`)
+				}, 250)
+			}, 1000)
 		}
 	}
 
