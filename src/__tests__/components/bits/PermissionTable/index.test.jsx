8217f4ffb1c62dc12530113d000cc78290cda0f4
anita908 <anita248846@gmail.com>
2020-10-08 12:59:57 -0600

    permission table test

--- src/__tests__/components/bits/PermissionTable/index.test.jsx
+++ src/__tests__/components/bits/PermissionTable/index.test.jsx
@@ -4,8 +4,11 @@ import PermissionTable from '../../../../components/bits/PermissionTable'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import * as testutil from '../../../testutil/testutil'
+import { Table, RemoveButton } from '../../../../components/bits/PermissionTable/styles'
 
-const data = {
+const data =
+[
+	{
 	email: `test@email.com`,
 	id: 22,
 	lastLogin: `2020-05-14T19:53:02.807Z`,
@@ -14,27 +17,30 @@ const data = {
 	roles: [`admin`],
 	username: `testusername`,
 	column: `Department`,
-}
+	},
+]
 
 const props = {
 	data,
 }
 
-describe(`content overview test`, () => {
+describe(`Permission Table test`, () => {
 	it(`mount`, () => {
-		const wrapper = mount(
+		let wrapper = mount(
 			<Provider store={testutil.store}>
 				<BrowserRouter>
 					<PermissionTable {...props} />
 				</BrowserRouter>
 			</Provider>,
 		)
+		const item = wrapper.find({"className": "item"}).at(0)
+		expect(item.text()).toEqual(`test@email.com`)
+	})
 
-		wrapper.find(`.tr`).forEach((node, index) => {
-				expect(node.text()).toEqual(`Department`)
-
-		})
-
-
+	it(`RemoveButton onClick`, ()=> {
+		const mockCallBack = jest.fn();
+		const button = shallow(<RemoveButton onClick={mockCallBack}/>);
+		button.find('StyledComponent').simulate('click');
+		expect(mockCallBack.mock.calls.length).toEqual(1);
 	})
 })
