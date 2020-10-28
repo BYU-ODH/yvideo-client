3e298c5a0838cc24b53206ce19a85e47a9c78e50
Esdras <eesc.gg@gmail.com>
2020-10-27 18:00:39 -0600

    Commented out function to prevent students from selecting other subtitles

--- src/components/bits/PlayerControls/index.jsx
+++ src/components/bits/PlayerControls/index.jsx
@@ -115,11 +115,22 @@ const PlayerControls = props => {
 					</div>
 				</div>
 			}
-			{ isCaption && (isAdmin || isProf) && 
+			{ isCaption && 
 				<div className="menu-modal" onMouseLeave={e => setIsCaption(false)}>
 					<h3>Select Caption</h3>
 					<div className="caption-list">
 						{/* Display all possible options with check mark for current selection. Add none field. */}
+						{subtitles.map((element, index) =>
+							<input key={element.id} type="button" value={element.language} onClick={e => handleChangeSubtitle(index)} className={ indexToDisplay == index ? ('active-value') : ('')}/>
+						)
+						}
+					</div>
+				</div>
+			}
+			{/* isCaption && (isAdmin || isProf) && 
+				<div className="menu-modal" onMouseLeave={e => setIsCaption(false)}>
+					<h3>Select Caption</h3>
+					<div className="caption-list">
 						{subtitles.map((element, index) =>
 							<input key={element.id} type="button" value={element.language} onClick={e => handleChangeSubtitle(index)} className={ indexToDisplay == index ? ('active-value') : ('')}/>
 						)
@@ -134,7 +145,7 @@ const PlayerControls = props => {
 						<input type="button" value={displaySubtitles.language} className={'active-value'}/>
 					</div>
 				</div>
-			}
+			}*/
 		</Style>
 	)
 
