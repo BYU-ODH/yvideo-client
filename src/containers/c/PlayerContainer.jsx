48fe2e582d2f19ec55952a485a8019f32ed7ed01
saejongjang <youthrich40@gmail.com>
2020-11-11 14:57:26 -0700

    added subtitle unit test

--- src/containers/c/PlayerContainer.jsx
+++ src/containers/c/PlayerContainer.jsx
@@ -27,6 +27,7 @@ const PlayerContainer = props => {
 	const params = useParams()
 
 	const [content, setContent] = useState()
+	const [resource, setResource] = useState(``)
 	const [sKey, setKey] = useState(``)
 
 	const [duration, setDuration] = useState(0) // Set duration of the media
@@ -46,11 +47,9 @@ const PlayerContainer = props => {
 	const [showTranscript, setShowTranscript] = useState(false)
 	const [toggleTranscript, setToggleTranscript] = useState(true)
 	const [subtitleText, setSubtitleText] = useState(``)
-	const [displaySubtitles, setDisplaySubtitles] = useState(null) //this is the subtitle that will show in the transcript view
+	const [displaySubtitles, setDisplaySubtitles] = useState(null) // this is the subtitle that will show in the transcript view
 
-	//this is for caption toggle
-	const [isCaption, setIsCaption] = useState( false ) //this is the state to toggle caption selection
-	const [indexToDisplay, setIndexToDisplay] = useState(0) //use index to display a desired subtitle based on selection from player controls.
+	const [isCaption, setIsCaption] = useState( content !== undefined ? content.settings.showCaptions : true )
 
 	const ref = player => {
 		setPlayer(player)
@@ -59,35 +58,26 @@ const PlayerContainer = props => {
 	useEffect(() => {
 		setPlaybackRate(1)
 		setShowTranscript(false)
-		setSubtitleText('')
-		setDisplaySubtitles(null)
 		if (!contentCache[params.id]){
-			//console.log('no cached content')
-			//get single content
+			// get single content?
 			getContent(params.id)
-		}
-		else {
-			//console.log('yes cached content')
+		} else {
 			setContent(contentCache[params.id])
 			setShowTranscript(contentCache[params.id].settings.showCaptions)
-			setKey('')
+			setKey(``)
 			setEvents(contentCache[params.id].settings.annotationDocument)
-			if(contentCache[params.id].url !== ''){
+			if(contentCache[params.id].url !== ``)
 				setUrl(contentCache[params.id].url)
-				getSubtitles(params.id)
-			}
 			else {
-				//CHECK RESOURCE ID
-				if(sKey === ''){
-					//VALID RESOURCE ID SO WE KEEP GOING TO FIND STREAMING URL
-					//console.log('ACTING TO CHANGE URL')
+				// CHECK RESOURCE ID
+				if(contentCache[params.id].resourceId !== `00000000-0000-0000-0000-000000000000` && sKey === ``){
+					// VALID RESOURCE ID SO WE KEEP GOING TO FIND STREAMING URL
+					setResource(contentCache[params.id].resourceId)
 					getStreamKey(contentCache[params.id].resourceId, contentCache[params.id].settings.targetLanguages)
 					setKey(streamKey)
-				}
-				else if (sKey !== ''){
-					//setUrl(`${process.env.REACT_APP_YVIDEO_SERVER}/api/media/stream-media/${sKey}`)
+				} else if (sKey !== ``){
+					// setUrl(`${process.env.REACT_APP_YVIDEO_SERVER}/api/media/stream-media/${sKey}`)
 					setUrl(`${process.env.REACT_APP_YVIDEO_SERVER}/api/partial-media/stream-media/${sKey}`)
-					getSubtitles(params.id)
 				}
 			}
 		}
@@ -124,15 +114,14 @@ const PlayerContainer = props => {
 	}
 
 	const handleProgress = progression => {
-		//console.log('progress', player.getCurrentTime())
 		setProgress(progression)
 	}
 
 	const handleSeekChange = (e, time) => {
-		//**TIME SHOULD BE A PERCENTAGE INSTEAD OF SECONDS */
+		//* *TIME SHOULD BE A PERCENTAGE INSTEAD OF SECONDS */
 		// const played = (e.clientX + document.body.scrollLeft) / window.innerWidth
 		// player.seekTo(played)
-		console.log('seeking', time, " seconds")
+		console.log(`seeking`, time, ` seconds`)
 		let newPlayed = 0
 		if(e !== null){
 			const scrubber = e.currentTarget.getBoundingClientRect()
@@ -141,58 +130,55 @@ const PlayerContainer = props => {
 			newPlayed = time / duration
 
 		if(newPlayed !== Infinity && newPlayed !== -Infinity){
-			console.log("in fraction: ", newPlayed)
+			console.log(`in fraction: `, newPlayed)
 			player.seekTo(newPlayed.toFixed(10), `fraction`)
 		}
 	}
 
 	const handleToggleFullscreen = () => {
 
-		//find the element which contains subtitles and events placeholders
-		let elem = document.getElementById('player-container')
+		// find the element which contains subtitles and events placeholders
+		const elem = document.getElementById(`player-container`)
 
-		//if fullscreen is false we want to turn to full screen. Else, request cancelFullScreen.
-		//For more info read full screen api https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API
-		//This is a functionality that behaves like F11. This is not video full screen mode.
-		//Video full screen mode would break the subtitles and events.
+		// if fullscreen is false we want to turn to full screen. Else, request cancelFullScreen.
+		// For more info read full screen api https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API
+		// This is a functionality that behaves like F11. This is not video full screen mode.
+		// Video full screen mode would break the subtitles and events.
 		if(!fullscreen){
 
-			if (elem.requestFullscreen) {
-				elem.requestFullscreen();
-			} else if (elem.mozRequestFullScreen) { /* Firefox */
-				elem.mozRequestFullScreen();
+			if (elem.requestFullscreen)
+				elem.requestFullscreen()
+			 else if (elem.mozRequestFullScreen) { /* Firefox */
+				elem.mozRequestFullScreen()
 			} else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-				elem.webkitRequestFullscreen();
+				elem.webkitRequestFullscreen()
 			} else if (elem.msRequestFullscreen) { /* IE/Edge */
-				elem.msRequestFullscreen();
+				elem.msRequestFullscreen()
 			}
 
-		}
-		else {
+		} else {
 
-			if (document.cancelFullScreen) {
-				document.cancelFullScreen();
-			} else if (document.mozCancelFullScreen) { /* Firefox */
-				document.mozCancelFullScreen();
+			if (document.cancelFullScreen)
+				document.cancelFullScreen()
+			 else if (document.mozCancelFullScreen) { /* Firefox */
+				document.mozCancelFullScreen()
 			} else if (document.webkitCancelFullScreen) { /* Chrome, Safari & Opera */
-				document.webkitCancelFullScreen();
+				document.webkitCancelFullScreen()
 			} else if (document.msExitFullscreen) { /* IE/Edge */
-				document.msExitFullscreen();
+				document.msExitFullscreen()
 			}
 		}
 
-		//set the state to whatever the state wasn't before.
-		//false to true && true to false.
+		// set the state to whatever the state wasn't before.
+		// false to true && true to false.
 		setFullscreen(!fullscreen)
 	}
 
 	const handleMuted = () => {
-		// console.log('calling mute', muted)
 		setMuted(true)
 	}
 
 	const handleUnmuted = () => {
-		// console.log('calling unmute', muted)
 		setMuted(false)
 	}
 
@@ -201,8 +187,6 @@ const PlayerContainer = props => {
 	}
 
 	const handleShowComment = (value, position) => {
-		// console.log(position)
-		// console.log('VALUE', value)
 		setVideoComment(value)
 		setCommentPosition(position)
 	}
@@ -213,18 +197,17 @@ const PlayerContainer = props => {
 	}
 
 	const handleChangeSubtitle = (index) => {
-		let temp = subtitles[index]
-		let currentContent = temp.content
+		const temp = subtitles[index]
+		const currentContent = temp.content
 
 		try {
 
-			if(typeof currentContent === "string"){
-				console.log("String type")
+			if(typeof currentContent === `string`){
+				console.log(`String type`)
 				temp.content = JSON.parse(subtitles[index].content)
 			}
 
-		}
-		catch (e){
+		} catch (e){
 			console.log(e)
 		}
 
@@ -235,32 +218,31 @@ const PlayerContainer = props => {
 	const handleShowHelp = () => {
 		toggleModal({
 			component: HelpDocumentation,
-			props: { name: 'Player'},
+			props: { name: `Player`},
 		})
 	}
 
 	if(displaySubtitles == null && content != undefined){
-		//This statement prevents displaySubtitles from being null.
-		//If displaySubtitles is null then the transcript list will be empty and no subtitles will be passed to the PlayerSubtitlesContainer
+		// This statement prevents displaySubtitles from being null.
+		// If displaySubtitles is null then the transcript list will be empty and no subtitles will be passed to the PlayerSubtitlesContainer
 
 		if(subtitles.length == 1){
-			//some logic to pick the subtitle
+			// some logic to pick the subtitle
 			handleChangeSubtitle(0)
-		}
-		else if(subtitles.length > 1) {
-			//pick the subtitle to display to be the one with the same language as the audio
-			let audioLanguage = content.settings.targetLanguages
+		} else if(subtitles.length > 1) {
+			// pick the subtitle to display to be the one with the same language as the audio
+			const audioLanguage = content.settings.targetLanguages
 
-			let result = 0;
+			let result = 0
 			for(let i = 0; i < subtitles.length; i++){
-				console.log("in loop")
-				let temp = subtitles[i];
-				console.log("TEMP CONTENT", temp)
-				//now that we have an actual object lets check language
-				//go through all subtitles and find there index where subtitle language = audio language
+				console.log(`in loop`)
+				const temp = subtitles[i]
+				console.log(`TEMP CONTENT`, temp)
+				// now that we have an actual object lets check language
+				// go through all subtitles and find there index where subtitle language = audio language
 				if(temp.language.toLowerCase() === audioLanguage.toLowerCase()){
-					result = i;
-					break;
+					result = i
+					break
 				}
 			}
 			handleChangeSubtitle(result)
