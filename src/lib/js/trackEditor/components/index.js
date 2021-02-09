123053e19bce45d8386f9a39981ae604a410960f
anita908 <anita248846@gmail.com>
2021-02-09 15:56:17 -0700

    TimeBar, Player, Status

--- src/lib/js/trackEditor/components/index.js
+++ src/lib/js/trackEditor/components/index.js
@@ -1,23 +1,14 @@
-import Caption from './c/Caption'
-import EventEditor from './c/EventEditor'
 import Player from './c/Player'
 import Status from './c/Status'
-import SuperScrollbar from './c/SuperScrollbar'
 import Timeline from './c/Timeline'
 import TrackEditor from './c/TrackEditor'
 import TrackHandler from './c/TrackHandler'
-import TrackMedia from './c/TrackMedia'
-import TimelineEditor from './c/TimelineEditor'
 
 export {
-	Caption,
-	EventEditor,
 	Player,
 	Status,
-	SuperScrollbar,
 	Timeline,
 	TrackEditor,
 	TrackHandler,
-	TrackMedia,
 	TimelineEditor,
 }
