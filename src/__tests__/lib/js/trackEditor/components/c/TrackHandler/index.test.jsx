7a9e2dfc25b3e3ea4ad76a5f85b11ac123ae8134
anita908 <anita248846@gmail.com>
2021-02-16 14:14:55 -0700

    trackEditor: Controls, TimeBar, Status, Timeline, TimelineEditor, TrackEditor, TrackHandler, TrackItemHandler

---
+++ src/__tests__/lib/js/trackEditor/components/c/TrackHandler/index.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { shallow, mount, render } from 'enzyme'
+import TrackHandler from '../../../../../../../lib/js/trackEditor/components/c/TrackHandler/index'
+import { BrowserRouter} from 'react-router-dom'
+
+
+describe(`TrackHandler test`, () => {
+	it(`TrackHandler`, ()=> {
+		let wrapper = mount(
+			<BrowserRouter>
+				<TrackHandler />
+			</BrowserRouter>,
+		)
+		console.log(wrapper.debug())
+	})
+})
--- src/lib/js/trackEditor/components/index.js
+++ src/lib/js/trackEditor/components/index.js
@@ -3,6 +3,7 @@ import Status from './c/Status'
 import Timeline from './c/Timeline'
 import TrackEditor from './c/TrackEditor'
 import TrackHandler from './c/TrackHandler'
+import TimelineEditor from './c/TimelineEditor'
 
 export {
 	Player,
