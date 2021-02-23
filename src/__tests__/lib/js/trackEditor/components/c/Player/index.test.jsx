0482ec82093ae8a968a69ab4bde335201abcf0df
anita908 <anita248846@gmail.com>
2021-02-23 00:31:00 -0700

    Player

--- src/__tests__/lib/js/trackEditor/components/c/Player/index.test.jsx
+++ src/__tests__/lib/js/trackEditor/components/c/Player/index.test.jsx
@@ -3,7 +3,6 @@ import { shallow, mount, render } from 'enzyme'
 import Player from '../../../../../../../lib/js/trackEditor/components/c/Player/index'
 import { BrowserRouter} from 'react-router-dom'
 
-
 const props = {
 	viewstate: {
 		ref : '',
@@ -23,13 +22,23 @@ const props = {
 
 describe(`Player test`, () => {
 	it(`Player`, ()=> {
-		let wrapper = mount(
-			<BrowserRouter>
-				<Player { ...props } />
-			</BrowserRouter>,
-		)
-		// console.log(wrapper.debug())
-		// wrapper.find('ReactPlayer').simulate('Duration', { duration: 12 })
-		wrapper.find("ReactPlayer").simulate('Progress')
+		let wrapper = shallow(<Player { ...props } />)
+		wrapper.find('ReactPlayer').simulate('ref', {ref: 'test'})
+		wrapper.find('ReactPlayer').simulate('Ready')
+		wrapper.find('ReactPlayer').simulate('Start')
+		wrapper.find('ReactPlayer').simulate('Play')
+		wrapper.find('ReactPlayer').simulate('Pause')
+		wrapper.find('ReactPlayer').simulate('Buffer')
+		wrapper.find('ReactPlayer').simulate('Seek')
+		wrapper.find('ReactPlayer').simulate('Error')
+		const mProgress = {
+			played: 12,
+			playedSeconds: 12,
+			loaded: 12,
+			loadedSeconds: 12,
+		}
+		const mDuration = 12
+		wrapper.find('ReactPlayer').simulate('Progress', {progress: mProgress})
+		wrapper.find('ReactPlayer').simulate('Duration', {duration: mDuration})
 	})
 })
