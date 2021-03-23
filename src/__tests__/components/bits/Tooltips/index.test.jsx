ad948031703be1d72d4fe6ea0ba0ca282589efb6
anita908 <anita248846@gmail.com>
2021-03-23 03:01:46 -0600

    tooltops test

--- src/__tests__/components/bits/Tooltips/index.test.jsx
+++ src/__tests__/components/bits/Tooltips/index.test.jsx
@@ -8,17 +8,30 @@ import * as testutil from '../../../testutil/testutil'
 const toolTip = testutil.toolTip
 
 const props = {
-	toolTip
+	tip: {
+		active: true,
+		props : {
+			name: 'help',
+			position: {
+				width: 20,
+				x: 136,
+				y: 108
+			}
+		},
+	}
 }
 
 describe(`Subtitles Layer test`, () => {
 	it(`mount`, () => {
-		let wrapper = mount(
+		const vElement = { style: { visibility: "visible" } }
+		const oElement = { style: { opacity: 1} }
+	 	const visibility = document.getElementById = jest.fn().mockReturnValueOnce(vElement)
+	 	const opacity = document.getElementById = jest.fn().mockReturnValueOnce(oElement)
+
+		let wrapper = shallow(
 			<Provider store={testutil.store}>
-				<BrowserRouter>
-					<ToolTip {...props} />
-				</BrowserRouter>
-			</Provider>,
+				<ToolTip {...props}/>
+			</Provider>, {attachTo: visibility}
 		)
 		console.log(wrapper.debug())
 	})
