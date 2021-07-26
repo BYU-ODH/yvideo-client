22577906b92261a1de45c726ca0c5bd35ab62a12
anita908 <anita248846@gmail.com>
2021-07-26 16:37:44 -0600

    overlaping

--- src/components/bits/SubtitlesCard/index.jsx
+++ src/components/bits/SubtitlesCard/index.jsx
@@ -8,19 +8,21 @@ import captions from 'assets/captions.svg'
 const SubtitlesCard = (props) => {
 
 	const { title, updateTitle, isEdit, subLayer, index } = props
+
 	return (
 		<Style>
 			<I src={captions}/>
 			{
 				isEdit ?
 					subLayer === index ?
-						<input type='text' className='sideTabInput' style={{margin: `0px`, width: `100%`}} value={title} onChange={e => {
-							updateTitle(e.target.value)
-						}}/>
+						<input type='text' className='sideTabInput' style={{margin: `0px`, width: `100%`}} value={title}
+							onChange={e =>updateTitle(e.target.value)}
+						/>
 						:
 						title
 					:
 					title
+
 			}
 		</Style>
 	)
