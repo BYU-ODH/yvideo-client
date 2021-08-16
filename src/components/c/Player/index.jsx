71c5504138fa49514ab197c9eb78931f0ef3134c
anita908 <anita248846@gmail.com>
2021-08-16 14:41:01 -0600

    set background color fix

--- src/components/c/Player/index.jsx
+++ src/components/c/Player/index.jsx
@@ -86,6 +86,7 @@ export default class Player extends PureComponent {
 
 		const handleOnProgress = ({ played, playedSeconds }) => {
 			const t0 = performance.now()
+			handleProgress(playedSeconds)
 			const subtitles = displaySubtitles
 			if(document.getElementById(`timeBarProgress`))
 				document.getElementById(`timeBarProgress`).style.width = `${played * 100}%`
@@ -167,7 +168,7 @@ export default class Player extends PureComponent {
 						<PlayerControls viewstate={this.props.viewstate} handlers={this.props.handlers}/>
 						<Blank blank={blank} id='blank' onContextMenu={e => e.preventDefault()}>
 							<PlayButton playing={playing} onClick={handlePlayPause} src={playButton} isMobile={isMobile} isLandscape={isLandscape}/>
-							<Subtitles style={{ display: `${subtitleText !== `` ? `flex` : `none`}` }} ><h3 id='subtitle'></h3></Subtitles>
+							<Subtitles style={{ display: `${subtitleText !== `` ? `flex` : `none`}` }} ><h3 subtitleText={subtitleText} id='subtitle'></h3></Subtitles>
 							<div id='censorContainer' style={{width:`100%`,height:`100%`,position:`absolute`,top:`0px`}}>
 							</div>
 							<div id ='commentContainer' style={{width:`100%`,height:`100%`,position:`absolute`,top:`0px`}}>
@@ -182,7 +183,7 @@ export default class Player extends PureComponent {
 					url !== `` && showTranscript ? (
 						// showsubtitles
 						<PlayerSubtitlesContainer
-							currentTime={progress.playedSeconds.toFixed(1)}
+							currentTime={progress}
 							duration={duration}
 							handleShowSubtitle={handleShowSubtitle}
 							indexToDisplay={indexToDisplay}
