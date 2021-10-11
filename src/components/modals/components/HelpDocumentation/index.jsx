cff2c6f582e5bbd262383efed69cd940508ca9c1
Esdras <eesc.gg@gmail.com>
2021-10-11 15:26:57 -0600

    Added video files for the tutorial for different parts of the application. Fixed functionality bugs in different pages'

--- src/components/modals/components/HelpDocumentation/index.jsx
+++ src/components/modals/components/HelpDocumentation/index.jsx
@@ -2,6 +2,8 @@ import React, { PureComponent } from 'react'
 
 import { Container, Back, CloseHelp, Tutorial } from './styles'
 
+import ReactPlayer from 'react-player'
+
 import closeIcon from 'assets/x.svg'
 
 export default class HelpDocumentation extends PureComponent {
@@ -23,8 +25,29 @@ export default class HelpDocumentation extends PureComponent {
 					<Container id='help-documentation-container' onScroll={this.handleScroll}>
 						<h1>{name} <CloseHelp onClick={this.props.toggleModal}><img src={closeIcon} /></CloseHelp></h1>
 						<div id='content'>
-							<Tutorial className="section"></Tutorial>
 						</div>
+						<div className="video-section">
+							<h2>{name} Video Tutorial</h2>
+							<div>
+								<video controls>
+									<source src={`/videos/${name.toLowerCase().replace(" ", "-")}.webm`} type="video/webm"/>
+								</video>
+							</div>
+						</div>
+						<br/>
+						{
+							name === "Manage Collections" ? (
+							<div className="video-section">
+								<h2>Manage Content Video Tutorial</h2>
+								<div>
+									<video controls>
+										<source src={`/videos/manage-content.webm`} type="video/webm"/>
+									</video>
+								</div>
+							</div>
+							) : (null)
+						}
+						<br/>
 					</Container>
 				</Back>
 			</>
