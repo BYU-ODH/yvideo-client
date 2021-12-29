d5cf3c73a850b2fb8a6ee474d862f5e668438f31
Esdras <eesc.gg@gmail.com>
2021-12-29 12:53:46 -0700

    Ordered contents alphabetically

--- src/components/bits/ListCollection/index.jsx
+++ src/components/bits/ListCollection/index.jsx
@@ -27,6 +27,9 @@ class ListCollection extends PureComponent {
 
 		const publishContent = content ? content.filter(item => item.published) : []
 
+
+		publishContent.sort((a, b) => {return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1})
+
 		if (!content || this.props.collection.published !== true ) return null
 
 		return (
