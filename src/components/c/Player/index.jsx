f4b2e20786c549e7c3ba9c7575873fb78767c528
Thanatos <aaron.andersonm@gmail.com>
2022-05-31 12:16:10 -0500

    fixes

--- src/components/c/Player/index.jsx
+++ src/components/c/Player/index.jsx
@@ -285,7 +285,7 @@ export default class Player extends Component {
 						<Blank blank={blank} id='blank' onContextMenu={e => e.preventDefault()}>
 							<PlayButton playing={playing} onClick={handlePlayPause} src={playButton} isMobile={isMobile} isLandscape={isLandscape}/>
 							{/* eslint-disable-next-line jsx-a11y/heading-has-content */}
-							<Subtitles style={{ display: `${subtitleText !== `` ? `flex` : `none`}` }} ><h3 subtitleText={subtitleText} id='subtitle'></h3></Subtitles>
+							<Subtitles style={{ display: `${subtitleText !== `` ? `flex` : `none`}` }} ><h3 id='subtitle'>{subtitleText}</h3></Subtitles>
 							<div id='censorContainer' style={{width:`100%`, height:`100%`, position:`absolute`, top:`0px`}}>
 							</div>
 							<div id ='commentContainer' style={{width:`100%`, height:`100%`, position:`absolute`, top:`0px`}}>
