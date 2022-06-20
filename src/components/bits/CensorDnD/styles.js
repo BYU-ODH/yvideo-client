dc7e468df78a43aa54c4342571459a12309647d7
Aaron Anderson <43586612+Aaron-M-Anderson@users.noreply.github.com>
2022-06-20 14:00:04 -0500

    Update styles.js

--- src/components/bits/CensorDnD/styles.js
+++ src/components/bits/CensorDnD/styles.js
@@ -42,27 +42,27 @@ import styled from 'styled-components'
 export const CloseBox = styled.div `
 	height: 8%;
 	width: auto;
-	background-color: rgba(255,255,255,0.95);
-	border: 2px solid rgba(5,130,202,0.8);
+	background-color: rgba(255, 255, 255, 0.95);
+	border: 2px solid rgba(5, 130, 202, 0.8);
 	border-radius: 5px;
 	position: absolute;
-	top:2%;
-	right:2%;
+	top: 2%;
+	right: 2%;
 	z-index: 502;
 	display: flex;
   justify-content: center;
   align-items: center;
 	& img {
-		width:100%;
-		height:100%;
+		width: 100%;
+		height: 100%;
 	}
 `
 export const BeforeButton = styled.div `
 	height: 10%;
 	width: 10%;
-	background-color: rgba(255,255,255,0.95);
-	border: 2px solid rgba(5, 130, 202,0.8);
-	border-radius:5px;
+	background-color: rgba(255, 255, 255, 0.95);
+	border: 2px solid rgba(5, 130, 202, 0.8);
+	border-radius: 5px;
 	position: absolute;
 	bottom: 2%;
 	left: 2%;
@@ -74,9 +74,9 @@ export const BeforeButton = styled.div `
 export const AfterButton = styled.div `
 	height: 10%;
 	width: 10%;
-	background-color: rgba(255,255,255,0.95);
-	border: 2px solid rgba(5, 130, 202,0.8);
-	border-radius:5px;
+	background-color: rgba(255, 255, 255, 0.95);
+	border: 2px solid rgba(5, 130, 202, 0.8);
+	border-radius: 5px;
 	position: absolute;
 	bottom: 2%;
 	right: 2%;
