276ade99bc429b4acf7df3b62d6aeac23637a8ef
Aaron Anderson <43586612+Aaron-M-Anderson@users.noreply.github.com>
2022-06-21 11:15:20 -0500

    Revert "config ios change"

--- src/components/c/Player/index.jsx
+++ src/components/c/Player/index.jsx
@@ -239,9 +239,6 @@ export default class Player extends Component {
 			}
 		}
 
-		const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
-
-
 		return (
 			<Style>
 				<div style={
@@ -269,17 +266,10 @@ export default class Player extends Component {
 							onSeek={e => e}
 							progressInterval={30}
 							onProgress={handleOnProgress}
+							// onProgressBar={handleOnReady}
 							onDuration={handleDuration}
 
 							config={{
-								file: {
-									forceHLS: !isSafari,
-									forceVideo: true,
-									hlsVersion: '0.12.4',
-									attributes: {
-										disablePictureInPicture: true
-									}
-								},
 								youtube: {
 									iv_load_policy: 3,
 									modestbranding: 1,
