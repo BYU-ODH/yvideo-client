078017305b1ba04924ed0da1dfe026c458840a32
Thanatos <aaron.andersonm@gmail.com>
2022-07-08 14:58:59 -0500

    LazyImage Test written

--- src/__tests__/components/bits/LazyImage/index.test.jsx
+++ src/__tests__/components/bits/LazyImage/index.test.jsx
@@ -1,6 +1,36 @@
-// stock-test: will be updated in like 1 day to be a real test
-describe(`stock test`, () => {
-	it(`stock test`, () => {
-		expect(1).toBe(1)
+import React from 'react'
+import LazyImage from '../../../../components/bits/LazyImage/index'
+import { render, screen, cleanup } from '@testing-library/react'
+
+const src = `test@src`
+const alt = `test`
+
+const wrapper =
+	<LazyImage src={src} alt={alt}/>
+
+const noImageWrapper =
+	<LazyImage />
+
+describe(`LazyImage test`, () => {
+	afterEach(() => {
+		jest.resetAllMocks()
+		cleanup()
+	})
+	it(`test render LazyImage with image given`, () => {
+		render(wrapper)
+		expect(wrapper).toBeDefined()
+
+		const image = screen.getByRole(`img`, { alt: /test/i })
+
+		expect(image).toBeDefined()
+	})
+
+	it(`test render LazyImage with no image given`, () => {
+		render(noImageWrapper)
+		expect(noImageWrapper).toBeDefined()
+
+		const image = screen.queryByRole(`img`, { alt: /test/i })
+
+		expect(image).toBeNull()
 	})
 })
