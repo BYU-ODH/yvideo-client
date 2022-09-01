6a1746436cacc0a85afddb8f2803ea1472b88161
Thanatos <aaron.andersonm@gmail.com>
2022-09-01 15:50:29 -0600

    small fix

--- src/components/c/Collections/index.jsx
+++ src/components/c/Collections/index.jsx
@@ -168,7 +168,7 @@ export default class Collections extends PureComponent {
 											})
 										}
 										onMouseLeave={toggleTip} />
-								}
+									}
 								</header>
 						}
 					</>
