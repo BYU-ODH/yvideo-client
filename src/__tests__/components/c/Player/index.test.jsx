c95542c24ba51ab0f983df8af001eb2216e3a754
Thanatos <aaron.andersonm@gmail.com>
2022-10-10 17:04:04 -0600

    Mild fix

--- src/__tests__/components/c/Player/index.test.jsx
+++ src/__tests__/components/c/Player/index.test.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { mount } from 'enzyme'
 import Player from '../../../../components/c/Player/index'
 import { BrowserRouter } from 'react-router-dom'
