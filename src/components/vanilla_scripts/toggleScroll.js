7b32e737661229ca75b6eb977db162638372007f
Aaron Anderson <aaron.andersonm@gmail.com>
2022-11-11 02:36:52 -0700

    fixed trackpad issue and also allowed vertical scroll on layers which need horizontal scroll disabled

--- src/components/vanilla_scripts/toggleScroll.js
+++ src/components/vanilla_scripts/toggleScroll.js
@@ -3,11 +3,11 @@ const handleScrollFuncs = (elements, setDisableScroll, setEnableScroll) => {
 
 	const keys = [`Space`, `ArrowUp`, `ArrowDown`, `ArrowLeft`, `ArrowRight`]
 
-	const preventDefault = (e) => {
+	const preventDefault = e => {
 		e.preventDefault()
 	}
 
-	const preventDefaultForScrollKeys = (e) => {
+	const preventDefaultForScrollKeys = e => {
 		keys.forEach((key) => {
 			if (e.code === key) {
 				preventDefault(e)
@@ -16,7 +16,12 @@ const handleScrollFuncs = (elements, setDisableScroll, setEnableScroll) => {
 		})
 	}
 
-	const preventDefaultShift = (e) => {
+	const preventDefaultTrackPad = e => {
+		if (Math.abs(e.deltaY) < 5)
+			preventDefault(e)
+	}
+
+	const preventDefaultShift = e => {
 		if(e.shiftKey === true)
 			preventDefault(e)
 	}
@@ -47,15 +52,13 @@ const handleScrollFuncs = (elements, setDisableScroll, setEnableScroll) => {
 
 	const wheelEvent = `onwheel` in document.createElement(`div`) ? `wheel` : `mousewheel`
 	const wheelOpt = supportsPassive ? { passive: false } : false
-
 	if(elements.length !== undefined) {
 		setDisableScroll({action: () => {
 			for(const i in elements) {
 				// these elements are the ones in the editors and the collections page
 				elements[i].addEventListener(`DOMMouseScroll`, preventDefault, false) // older FF
 				elements[i].addEventListener(wheelEvent, preventDefaultShift, wheelOpt) // modern desktop
-				elements[i].addEventListener(wheelEvent, preventDefault, wheelOpt)
-				elements[i].addEventListener(`touchmove`, preventDefault, wheelOpt) // mobile
+				elements[i].addEventListener(wheelEvent, preventDefaultTrackPad, wheelOpt) // mobile
 				elements[i].addEventListener(`keydown`, preventDefaultForScrollKeys, wheelOpt)
 				elements[i].onmousedown = e => { // disables middle mouse button
 					if (e.button === 1) return false
@@ -67,8 +70,7 @@ const handleScrollFuncs = (elements, setDisableScroll, setEnableScroll) => {
 				for(const i in elements) {
 					elements[i].removeEventListener(`DOMMouseScroll`, preventDefault, false)
 					elements[i].removeEventListener(wheelEvent, preventDefaultShift, wheelOpt)
-					elements[i].removeEventListener(wheelEvent, preventDefault, wheelOpt)
-					elements[i].removeEventListener(`touchmove`, preventDefault, wheelOpt)
+					elements[i].removeEventListener(wheelEvent, preventDefaultTrackPad, wheelOpt) // mobile
 					elements[i].removeEventListener(`keydown`, preventDefaultForScrollKeys, false)
 					elements[i].onmousedown = null
 				}
@@ -99,3 +101,101 @@ const handleScrollFuncs = (elements, setDisableScroll, setEnableScroll) => {
 }
 
 export default handleScrollFuncs
+
+// const handleScrollFuncs = (elements, setDisableScroll, setEnableScroll) => {
+// 	let supportsPassive = false
+
+// 	const keys = [`Space`, `ArrowUp`, `ArrowDown`, `ArrowLeft`, `ArrowRight`]
+
+// 	const preventDefault = (e) => {
+// 		e.preventDefault()
+// 	}
+
+// 	const preventDefaultForScrollKeys = (e) => {
+// 		keys.forEach((key) => {
+// 			if (e.code === key) {
+// 				preventDefault(e)
+// 				return false
+// 			}
+// 		})
+// 	}
+
+// 	const preventDefaultForMultiple = (e) => {
+// 		if(e.shiftKey === true)
+// 			preventDefault(e)
+// 	}
+
+// 	if(elements.length !== undefined) {
+// 		for(const i in elements) {
+// 			try {
+// 				elements[i].addEventListener(`test`, null, Object.defineProperty({}, `passive`, {
+// 					get: () => { // eslint-disable-line
+// 						supportsPassive = true
+// 					},
+// 				}))
+// 			} catch (e) {
+// 				return
+// 			}
+// 		}
+// 	} else {
+// 		try {
+// 			elements.addEventListener(`test`, null, Object.defineProperty({}, `passive`, {
+// 				get: () => { // eslint-disable-line
+// 					supportsPassive = true
+// 				},
+// 			}))
+// 		} catch (e) {
+// 			return
+// 		}
+// 	}
+
+// 	const wheelEvent = `onwheel` in document.createElement(`div`) ? `wheel` : `mousewheel`
+// 	const wheelOpt = supportsPassive ? { passive: false } : false
+
+// 	if(elements.length !== undefined) {
+// 		setDisableScroll({action: () => {
+// 			for(const i in elements) {
+// 				// these elements are the ones in the editors and the collections page
+// 				elements[i].addEventListener(`DOMMouseScroll`, preventDefault, false) // older FF
+// 				elements[i].addEventListener(wheelEvent, preventDefaultForMultiple, wheelOpt) // modern desktop
+// 				elements[i].addEventListener(`touchmove`, preventDefault, wheelOpt) // mobile
+// 				elements[i].addEventListener(`keydown`, preventDefaultForScrollKeys, wheelOpt)
+// 				elements[i].onmousedown = e => { // disables middle mouse button
+// 					if (e.button === 1) return false
+// 				}
+// 			}
+// 		}})
+// 		if(setEnableScroll) {
+// 			setEnableScroll({action: () => {
+// 				for(const i in elements) {
+// 					elements[i].removeEventListener(`DOMMouseScroll`, preventDefault, false)
+// 					elements[i].removeEventListener(wheelEvent, preventDefaultForMultiple, wheelOpt)
+// 					elements[i].removeEventListener(`touchmove`, preventDefault, wheelOpt)
+// 					elements[i].removeEventListener(`keydown`, preventDefaultForScrollKeys, false)
+// 					elements[i].onmousedown = null
+// 				}
+// 			}})
+// 		}
+// 	} else {
+// 		setDisableScroll({action: () => {
+// 			elements.addEventListener(`DOMMouseScroll`, preventDefault, false) // older FF
+// 			elements.addEventListener(wheelEvent, preventDefault, wheelOpt) // modern desktop
+// 			elements.addEventListener(`touchmove`, preventDefault, wheelOpt) // mobile
+// 			elements.addEventListener(`keydown`, preventDefaultForScrollKeys, false)
+// 			elements.onmousedown = e => { // disables middle mouse button
+// 				if (e.button === 1) return false
+// 			}
+// 		}})
+// 		if(setEnableScroll) {
+// 			setEnableScroll({action: () => {
+// 				elements.removeEventListener(`DOMMouseScroll`, preventDefault, false)
+// 				elements.removeEventListener(wheelEvent, preventDefault, wheelOpt)
+// 				elements.removeEventListener(`touchmove`, preventDefault, wheelOpt)
+// 				elements.removeEventListener(`keydown`, preventDefaultForScrollKeys, false)
+// 				elements.onmousedown = null
+// 			}})
+// 		}
+// 	}
+// }
+
+// export default handleScrollFuncs
